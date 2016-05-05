a53d0bf9a7a90d21f1475579edb6a3bf90cf73d6
Tj Carskadon <tjcarskadon@gmail.com>
2016-05-04 22:26:49 -0700

    pass all songQueueView prompts

--- client/env/config.example.js
+++
@@ -1,5 +0,0 @@
-// Put your parse application keys here!
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'OWawBR069jvFwWln4zrgOJR2wOCN6oetbBdvMLJq');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'GH78f2KLgWQT16v2OtQjI0WJZXBd7czwegMoIogB');
-});
